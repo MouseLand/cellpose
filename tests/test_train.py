f84f8706afc1ce1399b6fcee5c58d029f27ffc46
Carsen Stringer <carsen.stringer@gmail.com>
2020-09-27 11:39:44 -0400

    error with remove folder

--- tests/test_train.py
+++ tests/test_train.py
@@ -6,7 +6,7 @@ import os, shutil
 def test_class_train(data_dir, image_names):
     train_dir = str(data_dir.joinpath('2D').joinpath('train'))
     model_dir = str(data_dir.joinpath('2D').joinpath('train').joinpath('models'))
-    shutil.rmtree(model_dir)
+    shutil.rmtree(model_dir, ignore_errors=True)
     output = io.load_train_test_data(train_dir, mask_filter='_cyto_masks')
     images, labels, image_names, test_images, test_labels, image_names_test = output
     model = models.CellposeModel(pretrained_model=None, diam_mean=30)
@@ -18,7 +18,7 @@ def test_class_train(data_dir, image_names):
 def test_cli_train(data_dir, image_names):
     train_dir = str(data_dir.joinpath('2D').joinpath('train'))
     model_dir = str(data_dir.joinpath('2D').joinpath('train').joinpath('models'))
-    shutil.rmtree(model_dir)
+    shutil.rmtree(model_dir, ignore_errors=True)
     cmd = 'python -m cellpose --train --train_size --n_epochs 10 --dir %s --mask_filter _cyto_masks --pretrained_model cyto --chan 2 --chan2 1 --diameter 30'%train_dir
     try:
         cmd_stdout = check_output(cmd, stderr=STDOUT, shell=True).decode()
