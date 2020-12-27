85b788ebe594b0df50d7e6bf9475614e8bb815a6
Vini Salazar <viniws@gmail.com>
2020-12-27 14:17:55 -0300

    Applying Black formatting

--- imagej_roi_converter.py
+++ imagej_roi_converter.py
@@ -1,7 +1,7 @@
-from ij import IJ;
-from ij.plugin.frame import RoiManager;
-from ij.gui import PolygonRoi;
-from ij.gui import Roi;
+from ij import IJ
+from ij.plugin.frame import RoiManager
+from ij.gui import PolygonRoi
+from ij.gui import Roi
 from java.awt import FileDialog
 
 fd = FileDialog(IJ.getInstance(), "Open", FileDialog.LOAD)
@@ -14,16 +14,16 @@ rm = RM.getRoiManager()
 
 imp = IJ.getImage()
 
-textfile = open(file_name, 'r')
+textfile = open(file_name, "r")
 for line in textfile:
-	xy = map(int, line.rstrip().split(','))
-	X = xy[::2]
-	Y = xy[1::2]
-	imp.setRoi(PolygonRoi(X, Y, Roi.POLYGON));
-	#IJ.run(imp, "Convex Hull", "")
-	roi = imp.getRoi()
-	print(roi)
-	rm.addRoi(roi)
-textfile.close()  
-rm.runCommand("Associate", "true")	 
+    xy = map(int, line.rstrip().split(","))
+    X = xy[::2]
+    Y = xy[1::2]
+    imp.setRoi(PolygonRoi(X, Y, Roi.POLYGON))
+    # IJ.run(imp, "Convex Hull", "")
+    roi = imp.getRoi()
+    print(roi)
+    rm.addRoi(roi)
+textfile.close()
+rm.runCommand("Associate", "true")
 rm.runCommand("Show All with labels")
