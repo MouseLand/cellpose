da4941f47dbed0cb503c6ead2e4471fb120672bb
carsen <carsen.stringer@gmail.com>
2021-05-02 21:18:08 -0400

    fixing import for non-gui install

--- cellpose/__init__.py
+++ cellpose/__init__.py
@@ -1,4 +1,9 @@
 from .io import logger_setup
-from .gui import gui
-
+try:
+    from cellpose import gui 
+    GUI_ENABLED = True 
+except ImportError as err:
+    GUI_ERROR = err
+    GUI_ENABLED = False
+    
 logger, log_file = logger_setup()
