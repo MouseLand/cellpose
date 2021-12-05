cf0f0c74709a8194266caf0c0fcc1213e0f00124
Eric Jelli <eric.jelli@caesar.de>
2021-12-05 11:11:02 +0100

    test: Added empty label test for 'labels_to_flows'

--- tests/test_dynamics.py
+++ tests/test_dynamics.py
@@ -26,3 +26,16 @@ def test_masks_to_flows_gpu__empty():
     masks_to_flows_gpu(masks, dists)
 
     return
+
+def test_labels_to_flows__empty():
+    label_shape = (16, 16)
+    flow_shape = (4,) + label_shape
+
+    labels = [np.zeros(label_shape, dtype=int)]
+
+    flows = labels_to_flows(labels)
+    
+    assert all([flow.shape == flow_shape for flow in flows])
+    assert all([np.all(flow == 0) for flow in flows])
+
+    return
