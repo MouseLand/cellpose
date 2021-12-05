dd97d585f2cbe2f3531c8964654a03f9a080bd90
Eric Jelli <eric.jelli@caesar.de>
2021-12-05 10:50:43 +0100

    test: Check "reshape_train_test" with empty labels

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,3 +1,6 @@
+from cv2 import normalize
+import numpy as np
+
 from cellpose.transforms import *
 
 def test_random_rotate_and_resize__default():
@@ -11,3 +14,19 @@ def test_random_rotate_and_resize__use_skel():
     X = [np.random.rand(64, 64) for i in range(nimg)]
 
     random_rotate_and_resize(X, omni=True)
+
+def test_reshape_train_test__empty_labels():
+    img_size = (16, 16)
+    channels = (0, 0)
+    high_val = (2**16)-1
+    normalize = True
+    
+    train_data = [np.random.randint(0, high_val, size=img_size)]
+    train_labels = [np.zeros(img_size, dtype=int)]
+
+    test_data, test_labels = train_data, train_labels
+
+    reshape_train_test(
+        train_data, train_labels,
+        test_data, test_labels,
+        channels, normalize)
