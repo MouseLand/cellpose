71026d40478f3bbbbdb3464e75dd3e357a164fbc
Michael Rariden <raridenm@janelia.hhmi.org>
2025-04-21 16:29:22 -0400

    refactor main function and update image reading logic for 3D support

--- tests/test_output.py
+++ tests/test_output.py
@@ -95,7 +95,7 @@ def test_cli_2D(data_dir, image_names):
     clear_output(data_dir, image_names)
     use_gpu = torch.cuda.is_available()
     gpu_string = "--use_gpu" if use_gpu else ""
-    cmd = f"python -m cellpose --image_path {str(data_dir/"2D"/image_names[0])} --no_resample --diameter 0 --save_png --verbose {gpu_string}"
+    cmd = f"python -m cellpose --image_path {str(data_dir/"2D"/image_names[0])} --save_png --verbose {gpu_string}"
     try:
         cmd_stdout = check_output(cmd, stderr=STDOUT, shell=True).decode()
         print(cmd_stdout)
@@ -110,19 +110,15 @@ def test_cli_3D(data_dir, image_names_3d):
     clear_output(data_dir, image_names_3d)
     use_gpu = torch.cuda.is_available()
     gpu_string = "--use_gpu" if use_gpu else ""
-    model_types = ["cyto"]
-    chan = [2]
-    chan2 = [1]
-    for m, model_type in enumerate(model_types):
-        cmd = "python -m cellpose --image_path %s --do_3D --pretrained_model %s --no_resample --cellprob_threshold 0 --chan %d --chan2 %d --diameter 25 --save_tif %s" % (
-            str(data_dir / "3D" / image_names_3d[0]), model_type, chan[m], chan2[m], gpu_string)
-        try:
-            cmd_stdout = check_output(cmd, stderr=STDOUT, shell=True).decode()
-        except Exception as e:
-            print(e)
-            raise ValueError(e)
-        compare_masks(data_dir, image_names_3d[0], "3D")
-        clear_output(data_dir, image_names_3d)
+    cmd = f"python -m cellpose --image_path {str(data_dir / "3D" / image_names_3d[0])} --do_3D --diameter 25 --save_tif {gpu_string} --verbose"
+    try:
+        cmd_stdout = check_output(cmd, stderr=STDOUT, shell=True).decode()
+        print(cmd_stdout)
+    except Exception as e:
+        print(e)
+        raise ValueError(e)
+    compare_masks(data_dir, image_names_3d[0], "3D")
+    clear_output(data_dir, image_names_3d)
 
 def test_outlines_list(data_dir, image_names):
     """ test both single and multithreaded by comparing them"""
