629d8d8ce14dcfa658066aa94dc1217fdeb8dbaa
Cristian Goina <goinac@janelia.hhmi.org>
2025-12-11 11:45:52 -0500

    moving everything in one file

--- cellpose/contrib/distributed_segmentation.py
+++ cellpose/contrib/distributed_segmentation.py
@@ -4,12 +4,14 @@ import logging, os, tempfile, time, traceback
 # non-stdlib core dependencies
 import numpy as np
 import scipy
+import tifffile
 import torch
 
 
 # distributed dependencies
 import dask_image.ndmeasure as di_ndmeasure
 import dask_jobqueue
+import yaml
 import zarr
 
 
@@ -22,10 +24,348 @@ from cellpose.contrib.block_utils import (get_block_crops, get_nblocks, prepare_
 from dask.array.core import slices_from_chunks, normalize_chunks
 from dask.distributed import as_completed
 from typing import List
-from zarr.storage import LocalStore
 
 
 logger = logging.getLogger(__name__)
+
+
+######################## File format functions ################################
+def numpy_array_to_zarr(write_path, array, chunks):
+    """
+    Store an in memory numpy array to disk as a chunked Zarr array
+
+    Parameters
+    ----------
+    write_path : string
+        Filepath where Zarr array will be created
+
+    array : numpy.ndarray
+        The already loaded in-memory numpy array to store as zarr
+
+    chunks : tuple, must be array.ndim length
+        How the array will be chunked in the Zarr array
+
+    Returns
+    -------
+    zarr.core.Array
+        A read+write reference to the zarr array on disk
+    """
+
+    zarr_array = zarr.open(
+        write_path,
+        mode='w',
+        shape=array.shape,
+        chunks=chunks,
+        dtype=array.dtype,
+    )
+    zarr_array[...] = array
+    return zarr_array
+
+
+def wrap_folder_of_tiffs(
+    filename_pattern,
+    block_index_pattern=r'_(Z)(\d+)(Y)(\d+)(X)(\d+)',
+):
+    """
+    Wrap a folder of tiff files with a zarr array without duplicating data.
+    Tiff files must all contain images with the same shape and data type.
+    Tiff file names must contain a pattern indicating where individual files
+    lie in the block grid.
+
+    Distributed computing requires parallel access to small regions of your
+    image from different processes. This is best accomplished with chunked
+    file formats like Zarr and N5. This function can accommodate a folder of
+    tiff files, but it is not equivalent to reformating your data as Zarr or N5.
+    If your individual tiff files/tiles are huge, distributed performance will
+    be poor or not work at all.
+
+    It does not make sense to use this function if you have only one tiff file.
+    That tiff file will become the only chunk in the zarr array, which means all
+    workers will have to load the entire image to fetch their crop of data anyway.
+    If you have a single tiff image, you should just reformat it with the
+    numpy_array_to_zarr function. Single tiff files too large to fit into system
+    memory are not be supported.
+
+    Parameters
+    ----------
+    filename_pattern : string
+        A glob pattern that will match all needed tif files
+
+    block_index_pattern : regular expression string (default: r'_(Z)(\d+)(Y)(\d+)(X)(\d+)')
+        A regular expression pattern that indicates how to parse tiff filenames
+        to determine where each tiff file lies in the overall block grid
+        The default pattern assumes filenames like the following:
+            {any_prefix}_Z000Y000X000{any_suffix}
+            {any_prefix}_Z000Y000X001{any_suffix}
+            ... and so on
+
+    Returns
+    -------
+    zarr.core.Array
+    """
+
+    # define function to read individual files
+    def imread(fname):
+        with open(fname, 'rb') as fh:
+            return imagecodecs.tiff_decode(fh.read(), index=None)
+
+    # create zarr store, open it as zarr array and return
+    store = tifffile.imread(
+        filename_pattern,
+        aszarr=True,
+        imread=imread,
+        pattern=block_index_pattern,
+        axestiled={x:x for x in range(3)},
+    )
+    return zarr.open(store=store)
+
+
+######################## Cluster related functions ############################
+
+#----------------------- config stuff ----------------------------------------#
+DEFAULT_CONFIG_FILENAME = 'distributed_cellpose_dask_config.yaml'
+
+def _config_path(config_name):
+    """Add config directory path to config filename"""
+    return str(pathlib.Path.home()) + '/.config/dask/' + config_name
+
+
+def _modify_dask_config(
+    config,
+    config_name=DEFAULT_CONFIG_FILENAME,
+):
+    """
+    Modifies dask config dictionary, but also dumps modified
+    config to disk as a yaml file in ~/.config/dask/. This
+    ensures that workers inherit config options.
+    """
+    dask.config.set(config)
+    with open(_config_path(config_name), 'w') as f:
+        yaml.dump(dask.config.config, f, default_flow_style=False)
+
+
+def _remove_config_file(
+    config_name=DEFAULT_CONFIG_FILENAME,
+):
+    """Removes a config file from disk"""
+    config_path = _config_path(config_name)
+    if os.path.exists(config_path):
+        os.remove(config_path)
+
+
+#----------------------- clusters --------------------------------------------#
+class myLocalCluster(distributed.LocalCluster):
+    """
+    This is a thin wrapper extending dask.distributed.LocalCluster to set
+    configs before the cluster or workers are initialized.
+
+    For a list of full arguments (how to specify your worker resources) see:
+    https://distributed.dask.org/en/latest/api.html#distributed.LocalCluster
+    You need to know how many cpu cores and how much RAM your machine has.
+
+    Most users will only need to specify:
+    n_workers
+    ncpus (number of physical cpu cores per worker)
+    memory_limit (which is the limit per worker, should be a string like '16GB')
+    threads_per_worker (for most workflows this should be 1)
+
+    You can also modify any dask configuration option through the
+    config argument.
+
+    If your workstation has a GPU, one of the workers will have exclusive
+    access to it by default. That worker will be much faster than the others.
+    You may want to consider creating only one worker (which will have access
+    to the GPU) and letting that worker process all blocks serially.
+    """
+
+    def __init__(
+        self,
+        ncpus,
+        config={},
+        config_name=DEFAULT_CONFIG_FILENAME,
+        persist_config=False,
+        **kwargs,
+    ):
+        # config
+        self.config_name = config_name
+        self.persist_config = persist_config
+        scratch_dir = f"{os.getcwd()}/"
+        scratch_dir += f".{getpass.getuser()}_distributed_cellpose/"
+        config_defaults = {'temporary-directory':scratch_dir}
+        config = {**config_defaults, **config}
+        _modify_dask_config(config, config_name)
+
+        # construct
+        if "host" not in kwargs:
+            kwargs["host"] = ""
+        super().__init__(**kwargs)
+        self.client = distributed.Client(self)
+
+        # set environment variables for workers (threading)
+        environment_vars = {
+            'MKL_NUM_THREADS':str(ncpus),
+            'NUM_MKL_THREADS':str(ncpus),
+            'OPENBLAS_NUM_THREADS':str(ncpus),
+            'OPENMP_NUM_THREADS':str(ncpus),
+            'OMP_NUM_THREADS':str(ncpus),
+        }
+        def set_environment_vars():
+            for k, v in environment_vars.items():
+                os.environ[k] = v
+        self.client.run(set_environment_vars)
+
+        print("Cluster dashboard link: ", self.dashboard_link)
+
+    def __enter__(self): return self
+    def __exit__(self, exc_type, exc_value, traceback):
+        if not self.persist_config:
+            _remove_config_file(self.config_name)
+        self.client.close()
+        super().__exit__(exc_type, exc_value, traceback)
+
+
+class janeliaLSFCluster(dask_jobqueue.LSFCluster):
+    """
+    This is a thin wrapper extending dask_jobqueue.LSFCluster,
+    which in turn extends dask.distributed.SpecCluster. This wrapper
+    sets configs before the cluster or workers are initialized. This is
+    an adaptive cluster and will scale the number of workers, between user
+    specified limits, based on the number of pending tasks. This wrapper
+    also enforces conventions specific to the Janelia LSF cluster.
+
+    For a full list of arguments see
+    https://jobqueue.dask.org/en/latest/generated/dask_jobqueue.LSFCluster.html
+
+    Most users will only need to specify:
+    ncpus (the number of cpu cores per worker)
+    min_workers
+    max_workers
+    """
+
+    def __init__(
+        self,
+        ncpus,
+        min_workers,
+        max_workers,
+        config={},
+        config_name=DEFAULT_CONFIG_FILENAME,
+        persist_config=False,
+        **kwargs
+    ):
+
+        # store all args in case needed later
+        self.locals_store = {**locals()}
+
+        # config
+        self.config_name = config_name
+        self.persist_config = persist_config
+        scratch_dir = f"/scratch/{getpass.getuser()}/"
+        config_defaults = {
+            'temporary-directory':scratch_dir,
+            'distributed.comm.timeouts.connect':'180s',
+            'distributed.comm.timeouts.tcp':'360s',
+        }
+        config = {**config_defaults, **config}
+        _modify_dask_config(config, config_name)
+
+        # threading is best in low level libraries
+        job_script_prologue = [
+            f"export MKL_NUM_THREADS={2*ncpus}",
+            f"export NUM_MKL_THREADS={2*ncpus}",
+            f"export OPENBLAS_NUM_THREADS={2*ncpus}",
+            f"export OPENMP_NUM_THREADS={2*ncpus}",
+            f"export OMP_NUM_THREADS={2*ncpus}",
+        ]
+
+        # set scratch and log directories
+        if "local_directory" not in kwargs:
+            kwargs["local_directory"] = scratch_dir
+        if "log_directory" not in kwargs:
+            log_dir = f"{os.getcwd()}/dask_worker_logs_{os.getpid()}/"
+            pathlib.Path(log_dir).mkdir(parents=False, exist_ok=True)
+            kwargs["log_directory"] = log_dir
+
+        # graceful exit for lsf jobs (adds -d flag)
+        class quietLSFJob(dask_jobqueue.lsf.LSFJob):
+            cancel_command = "bkill -d"
+
+        # construct
+        super().__init__(
+            ncpus=ncpus,
+            processes=1,
+            cores=1,
+            memory=str(15*ncpus)+'GB',
+            mem=int(15e9*ncpus),
+            job_script_prologue=job_script_prologue,
+            job_cls=quietLSFJob,
+            **kwargs,
+        )
+        self.client = distributed.Client(self)
+        print("Cluster dashboard link: ", self.dashboard_link)
+
+        # set adaptive cluster bounds
+        self.adapt_cluster(min_workers, max_workers)
+
+
+    def __enter__(self): return self
+    def __exit__(self, exc_type, exc_value, traceback):
+        if not self.persist_config:
+            _remove_config_file(self.config_name)
+        self.client.close()
+        super().__exit__(exc_type, exc_value, traceback)
+
+
+    def adapt_cluster(self, min_workers, max_workers):
+        _ = self.adapt(
+            minimum_jobs=min_workers,
+            maximum_jobs=max_workers,
+            interval='10s',
+            wait_count=6,
+        )
+
+
+    def change_worker_attributes(
+        self,
+        min_workers,
+        max_workers,
+        **kwargs,
+    ):
+        """WARNING: this function is dangerous if you don't know what
+           you're doing. Don't call this unless you know exactly what
+           this does."""
+        self.scale(0)
+        for k, v in kwargs.items():
+            self.new_spec['options'][k] = v
+        self.adapt_cluster(min_workers, max_workers)
+
+
+#----------------------- decorator -------------------------------------------#
+def cluster(func):
+    """
+    This decorator ensures a function will run inside a cluster
+    as a context manager. The decorated function, "func", must
+    accept "cluster" and "cluster_kwargs" as parameters. If
+    "cluster" is not None then the user has provided an existing
+    cluster and we just run func. If "cluster" is None then
+    "cluster_kwargs" are used to construct a new cluster, and
+    the function is run inside that cluster context.
+    """
+    @functools.wraps(func)
+    def create_or_pass_cluster(*args, **kwargs):
+        # TODO: this only checks if args are explicitly present in function call
+        #       it does not check if they are set correctly in any way
+        assert 'cluster' in kwargs or 'cluster_kwargs' in kwargs, \
+        "Either cluster or cluster_kwargs must be defined"
+        if not 'cluster' in kwargs:
+            cluster_constructor = myLocalCluster
+            F = lambda x: x in kwargs['cluster_kwargs']
+            if F('ncpus') and F('min_workers') and F('max_workers'):
+                cluster_constructor = janeliaLSFCluster
+            with cluster_constructor(**kwargs['cluster_kwargs']) as cluster:
+                kwargs['cluster'] = cluster
+                return func(*args, **kwargs)
+        return func(*args, **kwargs)
+    return create_or_pass_cluster
 
 
 #----------------------- The main function -----------------------------------#
